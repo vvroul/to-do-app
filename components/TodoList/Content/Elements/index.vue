e1ad8bb60a3f9cd9ff5f44352f0a2642c73c53ff
Vrouliotis Vasileios <vvrouliotis@gmail.com>
2023-11-29 13:08:37 +0200

    Update api calls

--- components/TodoList/Content/Elements/index.vue
+++ components/TodoList/Content/Elements/index.vue
@@ -5,11 +5,12 @@
     >
       <input
         placeholder="Type task name"
+        autofocus
         class="italic text-[#bfbfbf] font-[Open_Sans] text-[17px] border-none w-[80%] bg-transparent"
         v-model="inputText"
       />
       <button
-        class="text-white mt-10 tablet:mt-0 px-12 py-3 relative overflow-hidden border-white-500 bg-black before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-white before:transition-all before:duration-300 hover:text-black hover:before:left-0 hover:before:w-full transform active:scale-75 transition-transform"
+        class="text-white mt-10 tablet:mt-0 px-12 py-3 relative overflow-hidden border-white-500 bg-black before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-amber-100 before:transition-all before:duration-300 hover:text-black hover:before:left-0 hover:before:w-full transform active:scale-75 transition-transform"
         @click="addItemToList()"
       >
         <span class="relative font-[Open_Sans]">ADD</span>
@@ -37,40 +38,54 @@ listItems.value = toRaw(items.value);
 // Add a new item to the list, binded to the input's text
 const addItemToList = async () => {
   if (inputText.value) {
-    await $fetch("https://to-do-app.titlos.com/todos", {
-      mode: "no-cors",
-      method: "post",
-      body: {
-        title: inputText.value,
-      },
-    });
+    const { data: newItem } = await useFetch(
+      "https://to-do-app.titlos.com/todos",
+      {
+        method: "post",
+        body: {
+          title: inputText.value,
+        },
+      }
+    );
+    // Push the new element to the items' list
+    listItems.value.push(toRaw(newItem.value));
+
+    //Clear the current input value
+    inputText.value = "";
   }
 };
 
 // Update an item from the list
 const updateItemFromList = async (itemId) => {
   if (itemId) {
-    await $fetch(`https://to-do-app.titlos.com/todos/${itemId}`, {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
+    await useFetch(`https://to-do-app.titlos.com/todos/${itemId}`, {
       method: "put",
       body: {
         completed: true,
       },
     });
+    // Update the corresponding element in the items' list
+    let index = listItems.value.findIndex((item) => {
+      return item.id === itemId;
+    });
+    listItems.value[index].completed = true;
   }
 };
 
 // Remove an item from the list
 const removeItemFromList = async (itemId) => {
   if (itemId) {
-    await $fetch(`https://to-do-app.titlos.com/todos/${itemId}`, {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
+    await useFetch(`https://to-do-app.titlos.com/todos/${itemId}`, {
       method: "delete",
     });
+
+    // Remove the corresponding element from the items' list
+    let index = listItems.value.findIndex((item) => {
+      return item.id === itemId;
+    });
+    if (index > -1) {
+      listItems.value.splice(index, 1);
+    }
   }
 };
 </script>
